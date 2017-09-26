6d96c4fb7a6dfa9df4ffc7258664c74c1af71d0f
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2017-09-26 16:12:59 +0100

    Create data_cleaning.py

---
+++ data_cleaning.py
@@ -0,0 +1,9 @@
+#py3!
+#import pandas as pd
+#import numpy as np
+#import matplotlib.pyplot as plt
+
+#fill the non applicable with zeros as these are weekends/public holidays 
+
+df_all = df_all.fillna(value=0)
+
